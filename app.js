0d0b1762b15650f1aef241338c25d56d602dc58c
Muhammed Ali <habipyilmaz@hotmail.com>
2021-09-13 00:02:31 +0300

    4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 5500
+
+const port = Process.env.PORT || 3000 ;
 
 app.get('/', (req, res) => {
   res.send('Haydi Bismillah !')
