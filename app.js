4c4d51aac751c02ae02bd8a08d813c109372328a
Muhammed Ali <habipyilmaz@hotmail.com>
2021-09-12 23:42:28 +0300

    hero

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.port || 5500
 
 app.get('/', (req, res) => {
-  res.send('Haydi Bismillah!')
+  res.send('Haydi Bismillah !')
 })
 
 app.listen(port, () => {
