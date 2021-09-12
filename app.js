a301b861bf1841c22d753d4136c5c5f44f001f15
Muhammed Ali <habipyilmaz@hotmail.com>
2021-09-12 23:18:52 +0300

    port

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5500
+const port = process.env.port || 5500
 
 app.get('/', (req, res) => {
   res.send('Haydi Bismillah!')
 })
 
 app.listen(port, () => {
-  console.log(`Örnek uygulama dinleniyor. http://localhost:${port}`)
+  console.log(`Örnek uygulama dinleniyor inşallah. http://localhost:${port}`)
 })
